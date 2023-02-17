e17a20ceb1b4f68a5e4b093f98f0d4b548d6d1da
wepukka <anssi.vepsalainen1@gmail.com>
2023-02-17 10:06:10 +0200

    Shop css changes, added sticky cart button

--- src/Views/Shop/Shop.jsx
+++ src/Views/Shop/Shop.jsx
@@ -1,15 +1,20 @@
 import("./Shop.css");
 import("../../assets/global.css");
-import ProductList from "./components/ScrollableItemList/ProductList";
 import shop1 from "../../assets/shop1.png";
 import shop2 from "../../assets/shop2.png";
 import shop3 from "../../assets/shop3.png";
+
+import SearchBar from "./components/SearchBar/SearchBar";
+import ProductList from "./components/ScrollableItemList/ProductList";
 import ClickableAdvertisement from "./components/ClickableAdvertisement/ClickableAdvertisement";
+import CartButton from "./components/CartButton/CartButton";
 
 export default function Shop() {
   return (
     <div className="shop">
       <div className="shop-container">
+        <CartButton /> 
+        <SearchBar /> 
         <ClickableAdvertisement images={[shop1, shop2, shop3, shop1, shop2]} />
         <ProductList />
       </div>
---
+++ src/Views/Shop/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import ("./SearchBar.css");
+
+export default function SearchBar()  {
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Search" />
+        </div>
+    )
+}   
+
