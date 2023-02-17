a13fd32418cac421c9d5aaf96eddeec659673a9d
Anssi Vepsäläinen <83569026+wepukka@users.noreply.github.com>
2023-02-17 08:15:45 +0200

    ScrollableAdvertisement to components, params now array of image paths

--- src/Views/Shop/Shop.jsx
+++ src/Views/Shop/Shop.jsx
@@ -1,36 +1,16 @@
 import("./Shop.css");
 import("../../assets/global.css");
-import { useEffect, useState } from "react";
 import ProductList from "./components/ScrollableItemList/ProductList";
 import shop1 from "../../assets/shop1.png";
 import shop2 from "../../assets/shop2.png";
 import shop3 from "../../assets/shop3.png";
+import ClickableAdvertisement from "./components/ClickableAdvertisement/ClickableAdvertisement";
 
 export default function Shop() {
-  const ClickableAdvertisement = (props) => {
-    return <img className={props.className} src={props.src} />;
-  };
-
   return (
     <div className="shop">
       <div className="shop-container">
-        <ul>
-          <li>
-            <ClickableAdvertisement className={"image-link"} src={shop1} />
-          </li>
-          <li>
-            <ClickableAdvertisement className={"image-link"} src={shop2} />
-          </li>
-          <li>
-            <ClickableAdvertisement className={"image-link"} src={shop2} />
-          </li>
-          <li>
-            <ClickableAdvertisement className={"image-link"} src={shop3} />
-          </li>
-          <li>
-            <ClickableAdvertisement className={"image-link"} src={shop3} />
-          </li>
-        </ul>
+        <ClickableAdvertisement images={[shop1, shop2, shop3, shop1, shop2]} />
         <ProductList />
       </div>
     </div>
