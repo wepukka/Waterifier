8716043e0c42d3fe7c45ea0a142dd09dcfb9e73e
Anssi Vepsäläinen <83569026+wepukka@users.noreply.github.com>
2023-02-21 15:18:15 +0200

    added link index link img

--- src/utils/manipulateArrays.jsx
+++ src/utils/manipulateArrays.jsx
@@ -1,15 +1,23 @@
-const sortProductsByProperty = (array, property, sortValue, saveValue) => {
-  if (sortValue === "asc") {
-    array.sort((a, b) => {
-      return a[property] - b[property];
-    });
-  } else if (sortValue === "desc") {
-    array.sort((a, b) => {
-      return b[property] - a[property];
-    });
+const sortProductsByProperty = async (
+  array,
+  property,
+  sortValue,
+  saveValue
+) => {
+  try {
+    if (sortValue === "asc") {
+      array.sort((a, b) => {
+        return a[property] - b[property];
+      });
+    } else if (sortValue === "desc") {
+      array.sort((a, b) => {
+        return b[property] - a[property];
+      });
+    }
+    return saveValue([...array]);
+  } catch (error) {
+    console.log(error);
   }
-
-  return saveValue([...array]);
 };
 
 export { sortProductsByProperty };
